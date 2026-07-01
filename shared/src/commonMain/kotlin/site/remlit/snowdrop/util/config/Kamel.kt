3386354e13c535695c1782c2ed1026db41b6b753
ihateblueb <ihateblueb@proton.me>
2026-07-01 15:17:46 -0400

    Fix slow Kamel image loading

--- shared/src/commonMain/kotlin/site/remlit/snowdrop/util/config/Kamel.kt
+++ shared/src/commonMain/kotlin/site/remlit/snowdrop/util/config/Kamel.kt
@@ -6,8 +6,6 @@ import io.kamel.core.config.httpUrlFetcher
 import io.kamel.core.config.takeFrom
 import io.kamel.image.config.animatedImageDecoder
 import io.kamel.image.config.Default
-import io.ktor.client.plugins.*
-import io.ktor.http.isSuccess
 
 val kamelConfig = KamelConfig {
 	takeFrom(KamelConfig.Default)
@@ -17,14 +15,5 @@ val kamelConfig = KamelConfig {
 	httpUrlFetcher {
 		// 100 MiB
 		httpCache(100 * 1024 * 1024)
-
-		install(HttpRequestRetry) {
-			retryOnServerErrors(maxRetries = 3)
-			exponentialDelay()
-
-			retryIf { _, httpResponse ->
-				!httpResponse.status.isSuccess()
-			}
-		}
 	}
 }
